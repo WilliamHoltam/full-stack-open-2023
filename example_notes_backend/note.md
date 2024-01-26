a8f1064bb13bfba9a7c1b3c999fd6f352c0e0364
Will <22566733+WilliamHoltam@users.noreply.github.com>
2024-01-26 22:16:41 +0000

    Attempt to improve example code and leave notes for myself.

---
+++ example_notes_backend/note.md
@@ -0,0 +1,6 @@
+# TODO
+
+1. Configure MongoDB and setup connection in .env file.
+2. Run node index.js locally and verify connection.
+3. Run frontend dev locally and verify that frontend can talk to backend. If not then do some research... either in course or online.
+4. Once this is working then try to get online deployment working.
--- example_notes_frontend/src/App.jsx
+++ example_notes_frontend/src/App.jsx
@@ -24,12 +24,31 @@ const App = () => {
       content: newNote,
       important: Math.random() > 0.5,
     }
-
+  
     noteService
       .create(noteObject)
         .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
         setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+        .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       })
   }
 
@@ -41,26 +60,6 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-   const toggleImportanceOf = id => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-
-      noteService
-        .update(id, changedNote).then(returnedNote => {
-          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-        })
-        .catch(error => {
-          setErrorMessage(
-            `Note '${note.content}' was already removed from server`
-          )
-          console.log(error)
-          setTimeout(() => {
-            setErrorMessage(null)
-          }, 5000)
-          setNotes(notes.filter(n => n.id !== id))
-        })
-    }
-
   return (
     <div>
       <h1>Notes</h1>
@@ -69,20 +68,21 @@ const App = () => {
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
-      </div>
+      </div>      
       <ul>
-        <ul>
-          {notesToShow.map(note =>
-            <Note
-              key={note.id}
-              note={note}
-              toggleImportance={() => toggleImportanceOf(note.id)}
-            />
-          )}
-        </ul>
+        {notesToShow.map(note => 
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
+      <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
         <button type="submit">save</button>
       </form>
       <Footer />
