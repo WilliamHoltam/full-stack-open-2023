96d191367b5a90181fda26423ac0a66fc6f1ff6e
William Holtam <22566733+WilliamHoltam@users.noreply.github.com>
2024-02-15 22:46:31 +0000

    completed 4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,28 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = listBlogs => {
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+
+    return listBlogs.length === 0
+        ? 0
+        : listBlogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = listBlogs => {
+    const reducer = (prev, current) => {
+        return prev.likes > current.likes ? prev : current
+    }
+
+    return listBlogs.length === 0
+        ? {}
+        : listBlogs.reduce(reducer)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
